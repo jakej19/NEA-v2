682f5e6481ea37d6133191548ea394f642d01b1a
18JJ0852 <18JJ0852@L2758>
2019-09-23 13:11:58 +0100

    output

--- matrix.vb
+++ matrix.vb
@@ -1,4 +1,4 @@
-Class matrix
+﻿Class matrix
     Private grid(,) As Double
     Private width As Integer
     Private height As Integer
@@ -15,5 +15,32 @@ Class matrix
         width = inx
         height = iny
         ReDim grid(width, height)
+    End Sub
+    Public Sub setVal(ByVal x As Integer, ByVal y As Integer, ByVal val As Double)
+        grid(x, y) = val
+    End Sub
+    Public Sub output()
+        'For i = 0 To y
+        '    For j = 0 To x
+        '        Console.Write(Math.Round(grid(j, i), 3) & " ")
+        '    Next
+        '    Console.WriteLine()
+        'Next
+        'Console.WriteLine()
+        Console.Write("┌")
+        Console.SetCursorPosition(4 * (x + 1) + 1, 0)
+        Console.WriteLine("┐")
+        For i = 0 To y
+            Console.Write("|")
+            For j = 0 To x
+                Console.Write(grid(j, i) & " ")
+                unhighlight()
+            Next
+            Console.Write("|")
+            Console.WriteLine()
+        Next
+        Console.Write("└")
+        Console.SetCursorPosition(4 * (x + 1) + 1, y + 2)
+        Console.WriteLine("┘")
     End Sub
 End Class
